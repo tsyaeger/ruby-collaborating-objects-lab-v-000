b01c48e18c801d8bdb209d5be2667ac850353667
taryn yaeger <tsyaeger@gmail.com>
2018-05-24 21:18:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
   
 
   def artist_name=(artist_name)
-    artist = Artist.find_or_create_by_name(artist_name)
+    self.artist = Artist.find_or_create_by_name(artist_name)
     @artist = artist
     artist.songs << self
   end
