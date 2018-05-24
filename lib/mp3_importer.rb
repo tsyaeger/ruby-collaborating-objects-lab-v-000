fa01af622eff4f412bbf56ca37103e434002686e
taryn yaeger <tsyaeger@gmail.com>
2018-05-24 21:00:27 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -18,7 +18,7 @@ class MP3Importer
   def import
     files
     puts files
-    song_objs = @files.each {|song_name| Song.new_by_filename(song_name)}
+    song_objs = files.each {|song_name| Song.new_by_filename(song_name)}
     puts song_objs.inspect
     song_objs
   end
